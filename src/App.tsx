52eb7ab140bbe81da032bd267a1067f7c6a3cecf
umpierres <alexandraumpierres@outlook.com>
2023-04-05 15:23:18 -0300

    adicionando stytles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import AppRoutes from "./routes/AppRoutes";
+import GlobalStyles from "./config/GlobalStyles";
 
 function App() {
-    return <AppRoutes />;
+    return (
+        <>
+            <AppRoutes />;
+            <GlobalStyles />
+        </>
+    );
 }
 
 export default App;
