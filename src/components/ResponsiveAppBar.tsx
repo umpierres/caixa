d4f3d93610ee7eb04f01b576f576b0537b6342e5
Jessica de Lurdes Fischer <jessicadelfischer@gmail.com>
2023-04-05 17:59:32 -0300

    Exclui o paper e adicionei no local container.

--- src/components/ResponsiveAppBar.tsx
+++ src/components/ResponsiveAppBar.tsx
@@ -57,7 +57,7 @@ function ResponsiveAppBar() {
               textDecoration: 'none'
             }}
           >
-            LOGO
+            Caixa
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
--- src/pages/CreateTransaction.tsx
+++ src/pages/CreateTransaction.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import { Button, Container, Grid, TextField, Typography } from '@mui/material';
 import ResponsiveAppBar from '../components/ResponsiveAppBar';
-import Paper from '../components/Paper';
 
 const CreateTransaction: React.FC = () => {
   return (
     <>
       <ResponsiveAppBar />
-      <Paper />
+      <Container>
+        <Grid container spacing={5}>
+          <Grid item xs={12}>
+            <Typography variant="h4">Saldo: R$ 0,00</Typography>
+          </Grid>
+          <Grid item lg={6} xs={6}>
+            <TextField label="Entrada" fullWidth type={'number'} id="outlined-size-small" size="small" />
+            <Button variant="contained" sx={{ marginTop: '7px' }}>
+              Adicionar
+            </Button>
+          </Grid>
+          <Grid item lg={6} xs={6}>
+            <TextField label="Saída" fullWidth type={'number'} id="outlined-size-small" size="small" />
+            <Button variant="contained" sx={{ marginTop: '7px' }}>
+              Retirar
+            </Button>
+          </Grid>
+        </Grid>
+      </Container>
     </>
   );
 };
