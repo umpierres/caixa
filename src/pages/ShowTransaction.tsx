657dcafbdcf8c04e9a11931c5d608328080f5601
umpierres <alexandraumpierres@outlook.com>
2023-04-05 14:48:08 -0300

    adicionando responsive app bar

--- src/pages/ShowTransaction.tsx
+++ src/pages/ShowTransaction.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import ResponsiveAppBar from "../components/ResponsiveAppBar";
 
 const ShowTransaction: React.FC = () => {
     return (
         <>
+            <ResponsiveAppBar />
             <h1>Growdev</h1>
         </>
     );
